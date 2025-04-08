3c634f1bf573678e45cb20f948ce3b0d14a864f3
zahra-yusuf <zahra.yusuf@mnsu.edu>
2025-04-08 09:43:09 -0500

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,10 +1,38 @@
 class TicTacToe:
   def __init__(self, board, player1, player2):
-    self.board=[" "] *9
-    self.player1 = ""
-    self.player2 = ""
+    self.board=[" "] * 9 #first instance variable
+    self.player1 = ""  #second instance variable
+    self.player2 = ""  #third instance variable
 
+  #shows the board
   def TheBoard(self):
     print("n\")
     print(self.board[0], "|", self.board[1], "|", self.board[2])
+    print(self.board[3], "|", self.board[4], "|", self.board[5])
+    print(self.board[6], "|", self.board[7], "|", self.board[8])
+
+   #gets a move from the player       
+  def get_move(self): 
+    move = int(input("pick a number 1-9"))
+    if move == "1":
+      return 0
+    elif move == "2":
+      return 1
+    elif move == "3":
+      return 2
+    elif move == "4":
+      return 3
+    elif move == "5":
+      return 4
+    elif move == "6":
+      return 5
+    elif move == "7":
+      return 6 
+    elif move == "8":
+      return 7
+    elif move == "9":
+      return 8
+    else:
+      print("Pick a different move.-- invalid number")
+  
 
