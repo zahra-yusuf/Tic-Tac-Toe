ed3009b8d3726385ad3d0ac8b666df8cb7b9f5ac
GeuAguek <tt7274sw@go.minnstate.edu>
2025-04-07 20:45:16 -0500

    Update Tic-Tac-Toe.py
    Keep adding what you can

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,1 +1,10 @@
+class TicTacToe:
+  def __init__(self, board, player1, player2):
+    self.board=[" "] *9
+    self.player1 = ""
+    self.player2 = ""
+
+  def TheBoard(self):
+    print("n\")
+
 
