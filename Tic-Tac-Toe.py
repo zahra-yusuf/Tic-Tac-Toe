f7ca795f0713d5ddee2d7ca454a0d7ba7eb2d1b2
nataliedehwe <natalie.dehwe@mnsu.edu>
2025-04-09 08:43:02 -0500

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -6,7 +6,7 @@ class TicTacToe:
     self.current = self.player1
   #shows the board
   def TheBoard(self):
-    print("n\")
+    print("\n")
     print(self.board[0], "|", self.board[1], "|", self.board[2])
     print(self.board[3], "|", self.board[4], "|", self.board[5])
     print(self.board[6], "|", self.board[7], "|", self.board[8])
