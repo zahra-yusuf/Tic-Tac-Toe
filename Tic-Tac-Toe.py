83e6ea49388f946187ce28aa2785c68e06b7ebc0
zahra-yusuf <zahra.yusuf@mnsu.edu>
2025-04-24 20:50:24 -0500

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -8,7 +8,9 @@ class TicTacToe:
   def TheBoard(self):  
     print("\n")
     print(self.board[0], "|", self.board[1], "|", self.board[2])
+    print("-------------------------------------------------")
     print(self.board[3], "|", self.board[4], "|", self.board[5])
+    print("-------------------------------------------------")
     print(self.board[6], "|", self.board[7], "|", self.board[8])
 
    #gets a move from the player       
